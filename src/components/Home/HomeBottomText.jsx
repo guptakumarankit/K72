1df4950f168fa6aea98d93b1292267c87b5ff35e
AnkitGupta <ankit.kr778389@gmail.com>
2025-10-11 18:26:53 +0530

    push

--- src/components/Home/HomeBottomText.jsx
+++ src/components/Home/HomeBottomText.jsx
@@ -5,7 +5,7 @@ const HomeBottomText = () => {
   return (
     <div className='flex items-center justify-center gap-2 font-[font2] pt-2'>
        <div className='border-3 h-24 flex items-center justify-center px-10 border-white rounded-full uppercase'>
-        <Link className='text-[6.5vw] mt-6' to='/projects'>Projects</Link>
+        <Link className='text-[6.5vw] mt-6' to='/project'>Projects</Link>
       </div>
       <div className='border-3 h-24 flex items-center justify-center px-10 border-white rounded-full uppercase'>
         <Link className='text-[6.5vw] mt-6' to='/agency'>Agency</Link>
